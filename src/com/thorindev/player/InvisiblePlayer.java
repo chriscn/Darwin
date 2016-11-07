c5344e63649de848eafb6eaf7481add537460d82
Chris Nethercott <c.nethercott@icloud.com>
2016-11-07 12:48:02 +0000

    Added VanishPlayer stuff

--- src/com/thorindev/player/InvisiblePlayer.java
+++ src/com/thorindev/player/InvisiblePlayer.java
@@ -1,4 +1,43 @@
 package com.thorindev.player;
 
-public class InvisiblePlayer {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.ArrayList;
+
+public class InvisiblePlayer implements Listener {
+
+    public static ArrayList<Player> vanished = new ArrayList<Player>();
+
+    public void VanishPlayer(Player player) {
+        for(Player toHide : Bukkit.getServer().getOnlinePlayers()) {
+            toHide.hidePlayer(player);
+        }
+        vanished.add(player);
+    }
+
+    public void unVanishPlayer(Player player) {
+        for(Player toShow : Bukkit.getServer().getOnlinePlayers()) {
+            toShow.showPlayer(player);
+        }
+        vanished.remove(player);
+    }
+
+    @EventHandler
+    public void onPlayerJoinVanish(PlayerJoinEvent event) {
+        for(Player player : vanished) {
+            event.getPlayer().hidePlayer(player);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuitVanish(PlayerQuitEvent event) {
+        if(vanished.contains(event.getPlayer())) {
+            vanished.remove(event.getPlayer());
+        }
+    }
 }
