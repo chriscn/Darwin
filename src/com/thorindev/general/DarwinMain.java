31e7e666609bbc8d8e1e8ba4ce484225ff26d736
Chris Nethercott <c.nethercott@icloud.com>
2016-11-07 07:58:23 +0000

    Create DarwinMain.java

---
+++ src/com/thorindev/general/DarwinMain.java
@@ -0,0 +1,15 @@
+package com.thorindev.general;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DarwinMain extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
