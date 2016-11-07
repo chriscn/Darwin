c16ef22487075dcf554bb77ba1ae618e6772ad31
Chris Nethercott <c.nethercott@icloud.com>
2016-11-07 10:28:34 +0000

    Added enchantItemInHand

--- src/com/thorindev/item/EnchantItem.java
+++ src/com/thorindev/item/EnchantItem.java
@@ -2,6 +2,14 @@ package com.thorindev.item;
 
 public class EnchantItem {
 
+    public void enchantItemInHand(Player player, Enchantment enchantment, int level) {
+        ItemStack item = player.getInventory().getItemInMainHand();
+        ItemStack air = new ItemStack(Material.AIR);
+        if (item != air) {
+            item.addEnchantment(enchantment, level);
+        }
+    }
+
 
 
 }
