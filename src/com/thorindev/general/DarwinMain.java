01886ebd969e9d286da94d4eeb3ac2098920c95e
Chris Nethercott <c.nethercott@icloud.com>
2016-11-07 08:01:15 +0000

    Added some stuff to darwin main

--- src/com/thorindev/general/DarwinMain.java
+++ src/com/thorindev/general/DarwinMain.java
@@ -1,8 +1,17 @@
 package com.thorindev.general;
 
+import com.thorindev.entity.FireworkAPI;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DarwinMain extends JavaPlugin {
+public class DarwinMain extends JavaPlugin implements Listener {
+
+    FireworkAPI fw = new FireworkAPI();
+
     @Override
     public void onEnable() {
 
@@ -10,6 +19,16 @@ public class DarwinMain extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        
+
+    }
+
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if(!player.hasPlayedBefore()) {
+            player.sendMessage(ChatColor.GREEN + "Hello there! I see that you haven't played before, have a firework");
+            fw.spawnRandomFirework(player);
+        }
     }
 }
