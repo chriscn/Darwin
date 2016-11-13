27cb935a35be20e8ee15dee6963c4b973ce8a514
Chris <c.nethercott@icloud.com>
2016-11-13 20:12:21 +0000

    I give up

--- src/com/thorindev/DarwinMain.java
+++ src/com/thorindev/DarwinMain.java
@@ -4,14 +4,11 @@ import com.thorindev.entity.EntityMain;
 import com.thorindev.item.ItemMain;
 import com.thorindev.player.PlayerMain;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Achievement;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerAchievementAwardedEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -48,22 +45,9 @@ public class DarwinMain extends JavaPlugin implements Listener {
         return true;
     }
 
-    @EventHandler
-    public void onPlayerJoinEvent(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        if(player.hasPlayedBefore()) {
-            player.sendMessage(ChatColor.GREEN + "Hello there! I see that you haven't played before, have a firework");
-            entityMain.fireworkEntity.spawnRandomFirework(player);
-        }
-    }
-
-    @EventHandler
-    public void onPlayerGetAchievement(PlayerAchievementAwardedEvent event) {
-        Player player = event.getPlayer();
-        Achievement eventAchievement = event.getAchievement();
-        if(event.getAchievement() == Achievement.OPEN_INVENTORY) {
-            player.sendMessage(ChatColor.GREEN + "Well done for opening your inventory!");
-            itemMain.giveItem.giveItem(player, Material.DIAMOND, 1);
-        }
-    }
+   @EventHandler
+   public void onPlayerJoinEvent(PlayerJoinEvent event) {
+       Player player = event.getPlayer();
+       playerMain.playerHotbar.sendActionBar(player, "Hello World");
+   }
 }
