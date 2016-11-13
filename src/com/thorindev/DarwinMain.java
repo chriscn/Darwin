b8a4a436d332eb09fd44d48b2de27b608275017b
Chris Nethercott <c.nethercott@icloud.com>
2016-11-13 20:11:05 +0000

    Added and removed some stuff

--- src/com/thorindev/DarwinMain.java
+++ src/com/thorindev/DarwinMain.java
@@ -12,7 +12,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAchievementAwardedEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DarwinMain extends JavaPlugin implements Listener {
@@ -23,7 +22,7 @@ public class DarwinMain extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-
+        getLogger().info("DarwinMain has been enabled");
     }
 
     @Override
@@ -49,15 +48,6 @@ public class DarwinMain extends JavaPlugin implements Listener {
                 break;
         }
         return true;
-    }
-
-    @EventHandler
-    public void onPlayerJoinEvent(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        if(player.hasPlayedBefore()) {
-            player.sendMessage(ChatColor.GREEN + "Hello there! I see that you haven't played before, have a firework");
-            entityMain.fireworkEntity.spawnRandomFirework(player);
-        }
     }
 
     @EventHandler
