65f27bd539a1decbcccee45175053ce2fa35563e
Chris Nethercott <c.nethercott@icloud.com>
2016-11-27 19:38:11 +0000

    The custom colour class allows you to get the byte value of a colour just via its name

---
+++ src/com/thorindev/item/CustomColor.java
@@ -0,0 +1,30 @@
+package com.thorindev.item;
+
+public enum CustomColor {
+
+    BLACK(0), //ink sack
+    RED(1),
+    GREEN(2),
+    BROWN(3),
+    BLUE(4), //lapis lazuli
+    PURPLE(5),
+    CYAN(6),
+    LIGHT_GRAY(7),
+    GRAY(8),
+    PINK(9),
+    LIME(10),
+    YELLOW(11),
+    LIGHT_BLUE(12),
+    MAGENTA(13),
+    ORANGE(14),
+    WHITE(15); //bonemeal
+
+    private byte data;
+    CustomColor(int i){
+        this.data = (byte) i;
+    }
+
+    public byte getData(){
+        return this.data;
+    }
+}
