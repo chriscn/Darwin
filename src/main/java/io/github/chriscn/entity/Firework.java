4e14c5347377184de3e43022766ec16292a2c03a
Chris Nethercott <ccnethercott@gmail.com>
2017-08-16 20:26:23 +0100

    arrays and stuf

--- src/main/java/io/github/chriscn/entity/Firework.java
+++ src/main/java/io/github/chriscn/entity/Firework.java
@@ -1,4 +1,58 @@
 package io.github.chriscn.entity;
 
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+
+import java.util.Random;
+
 public class Firework {
+
+    Random random = new Random();
+
+    
+
+    public Color getFWColor() {
+        Color c;
+        Color[] listOfColors = {
+                Color.AQUA,
+                Color.BLACK,
+                Color.BLUE,
+                Color.FUCHSIA,
+                Color.GREEN,
+                Color.GRAY,
+                Color.LIME,
+                Color.MAROON,
+                Color.NAVY,
+                Color.OLIVE,
+                Color.ORANGE,
+                Color.PURPLE,
+                Color.RED,
+                Color.SILVER,
+                Color.TEAL,
+                Color.WHITE,
+                Color.YELLOW,
+        };
+        int i = random.nextInt(listOfColors.length);
+        c = listOfColors[i];
+        return c;
+    }
+
+    public FireworkEffect.Type getFWType() {
+        FireworkEffect.Type t;
+        FireworkEffect.Type[] listOfTypes = {
+                FireworkEffect.Type.BALL,
+                FireworkEffect.Type.BALL_LARGE,
+                FireworkEffect.Type.BURST,
+                FireworkEffect.Type.CREEPER,
+                FireworkEffect.Type.STAR,
+        };
+        int i = random.nextInt(listOfTypes.length);
+        t = listOfTypes[i];
+        return t;
+    }
+
+    public int getFWPower() {
+        return random.nextInt(128);
+    }
+
 }
