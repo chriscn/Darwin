c45acb93a52fb6ce06fc54702364894b2a161f3e
Chris Nethercott <ccnethercott@gmail.com>
2017-08-16 14:17:54 +0100

    for some reason they aren't the same

---
+++ src/main/java/io/github/chriscn/util/GlassColor.java
@@ -0,0 +1,31 @@
+package io.github.chriscn.util;
+
+public enum GlassColor {
+
+    WHITE(0),
+    ORANGE(1),
+    MAGENTA(2),
+    LIGHT_BLUE(3),
+    YELLOW(4),
+    LIME(5),
+    PINK(6),
+    GRAY(7),
+    LIGHT_GRAY(8),
+    CYAN(9),
+    PURPLE(10),
+    BLUE(11),
+    BROWN(12),
+    GREEN(13),
+    RED(14),
+    BLACK(15);
+
+    private byte data;
+
+    GlassColor(int i) {
+        this.data = (byte) i;
+    }
+
+    public byte getColor() {
+        return this.data;
+    }
+}
